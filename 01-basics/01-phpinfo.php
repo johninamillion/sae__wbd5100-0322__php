81016015a9a66b9d9f83e38814d3da42c7e0469b
johninamillion <johninamio@gmail.com>
2022-06-13 22:57:56 +0200

    basics - phpinfo

---
+++ 01-basics/01-phpinfo.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * Liefert Informationen zur PHP Konfiguration
+ *
+ * php.net Referenz
+ * https://www.php.net/manual/de/function.phpinfo.php
+ *
+ * Wichtigste Konfigurationen (im Bezug auf den Unterricht):
+ * display_errors       legt fest ob Errormeldungen dargestellt werden
+ * file_uploads         legt fest ob Dateiuploads zulässig sind
+ * max_file_uploads     legt die maximale Anzahl paralleler Uploads fest
+ * upload_max_filesize  legt die maximale Dateigröße von einem Upload fest
+ *
+ * Wichtige Bibliotheken (im Bezug auf den Unterricht):
+ * GD       wird verwendet für Bildbearbeitung mit Hilfe von PHP
+ * https://www.php.net/manual/de/book.image.php
+ * gettext  wird verwendet zur Internationalisierung von PHP Anwendungen
+ * https://www.php.net/manual/de/book.gettext.php
+ * mysqli   wird verwendet um mit PHP auf eine MySQL Datenbank zuzugreifen (PDO wird empfohlen)
+ * https://www.php.net/manual/de/book.mysqli.php
+ * PDO      wird verwendet um mit PHP auf Datenbanken zuzugreifen
+ * https://www.php.net/manual/de/book.pdo.php
+ */
+phpinfo();
