ff9c005b7b2e617ec90515a9e3d4864c37356808
johninamillion <johninamio@gmail.com>
2022-06-13 23:24:30 +0200

    basics - print_r

--- 01-basics/04-print-r.php
+++ 01-basics/04-print-r.php
@@ -4,22 +4,23 @@ error_reporting( E_ALL );
 ini_set( 'display_errors', '1' );
 
 /*
- * Gibt einen Wert aus und Rückgabewert
+ * Gibt einen Wert in einer lesbaren Form aus
  *
- * Geeignet für Strings, Integers und Floats
+ * php.net Referenz:
+ * https://www.php.net/manual/de/function.print-r.php
+ *
+ * Geeignet für Strings, Integers, Floats, Arrays und Objects
  * String       gibt den String aus
  * Integer      gibt den Integer aus
  * Float        gibt den Float aus
+ * Array        gibt den Array mit index/kex => value aus
+ * Object       gibt den Klassennamen des Objektes und die Objektvariablen mit Geltungsbereich aus
  *
- * Nicht geeignet für Booleans, Arrays und Objects
+ * Nicht geeignet für Booleans
  * Booleans     gibt bei TRUE 1 aus, bei FALSE keine Ausgabe
- * Array        gibt "Array" aus
- * Object       gibt den Klassennamen des Objektes aus
  */
-print "Hello World!";   // Ausgabe von einem String
-print 1;                // Ausgabe von einem Integer
-print 1.2;              // Ausgabe von einem Float
-
-print( "Hello World" ); // Alternative Schreibweise zur Ausgabe von einem String
-print( 1 );             // Alternative Schreibweise zur Ausgabe von einem Integer
-print( 1.2 );           // Alternative Schreibweise zur Ausgabe von einem Float
+print_r( "Hello World" );   // Ausgabe von einem String
+print_r( 1 );               // Ausgabe von einem Integer
+print_r( 1.2 );             // Ausgabe von einem Float
+print_r( [ 'a', 'b', 'c' ] );     // Ausgabe von einem Array
+print_r( new Exception() );
